6719bcb8685d48236fc844e564c6e74ed45a285c
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-04 17:31:59 +0100

    Update Serie.java

--- Serie.java
+++ Serie.java
@@ -1,1 +1,37 @@
+package dk.ns.series;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Serie {
+    private String titre;
+    private List<Saison> saisons;
+	
+    public Serie(String titre) {
+		super();
+		this.titre = titre;
+		this.saisons = new ArrayList<>();
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public List<Saison> getSaisons() {
+		return saisons;
+	}
+
+    public void ajouterSaison(Saison saison) {
+        saisons.add(saison);
+	}
+
+	@Override
+	public String toString() {
+		return "Serie [titre=" + titre + ", saisons=" + saisons + "]";
+	}
+}
 
